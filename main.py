3dd815d91c36ce4ac49f21a80cb7a063aaa4ed92
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-03-22 15:30:45 +0545

    testing

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ import os
 load_dotenv()
 app = FastAPI()
 
-client = Groq(api_key=os.getenv("API_KEY"))
-
-class Chat(BaseModel):
-  message:str
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:8080"],  # Anyone can access
@@ -21,43 +15,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-
-@app.post('/chat/')
-def chat(data:Chat):
-
-    full_response = ""
-
-    completion = client.chat.completions.create(
-    model="openai/gpt-oss-120b",
-    messages=[
-       {
-          "role":"system",
-          "content":"You are an travelling ai helper deciding to choose destiny for users to travel mainly focused on nepal travel your name is Rocket and you were made by Team Rocket"
-       },
-      {
-         
-        "role": "user",
-        "content": data.message
-      }
-    ],
-    temperature=1,
-    max_completion_tokens=8192,
-    top_p=1,
-    reasoning_effort="medium",
-    stream=False,
-    stop=None
-)
-
-    for chunk in completion:
-     data = chunk.choices[0].delta.content 
-     if data:
-        full_response +=data
-
-    response = {
-       'response':full_response
-    }
-    return response
-
+app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
